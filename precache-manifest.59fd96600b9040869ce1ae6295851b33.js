10a6b62c3c71718f94e92732709e618fedbe9a79
Janusz Omyliński <jomylinski@grupa.onet>
2019-12-11 21:41:54 +0100

    Updates

---
+++ precache-manifest.59fd96600b9040869ce1ae6295851b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0993e40b67f9171d2573b8a345cbcefa",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7b90e85fc44327838cfb",
+    "url": "/weather/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e3e7a0e363133725df52",
+    "url": "/weather/static/css/main.d28e6927.chunk.css"
+  },
+  {
+    "revision": "7b90e85fc44327838cfb",
+    "url": "/weather/static/js/2.b4b3d95c.chunk.js"
+  },
+  {
+    "revision": "e3e7a0e363133725df52",
+    "url": "/weather/static/js/main.3de99e6b.chunk.js"
+  },
+  {
+    "revision": "bcbbf36160edab700d41",
+    "url": "/weather/static/js/runtime-main.ee98893b.js"
+  }
+]);
